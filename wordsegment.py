85bb39acc6012bd92005e9a5e4b7a002039771db
Grant Jenks <contact@grantjenks.com>
2015-09-07 12:30:13 -0500

    Bump version to 0.5.3

--- wordsegment.py
+++ wordsegment.py
@@ -141,8 +141,8 @@ if __name__ == '__main__':
     main(sys.argv[1:])
 
 __title__ = 'wordsegment'
-__version__ = '0.5.2'
-__build__ = 0x000502
+__version__ = '0.5.3'
+__build__ = 0x000503
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015 Grant Jenks'
