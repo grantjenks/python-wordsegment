416cd6e8b844c05e79965fe176203a54f9eb4d04
Grant Jenks <contact@grantjenks.com>
2016-12-20 16:10:42 -0800

    Add Python 2 and 3.5 classifiers

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import sys
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
@@ -43,11 +41,13 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
