afdc177125e79caeeae90df0e5e29238140b44e0
Grant Jenks <contact@grantjenks.com>
2017-09-29 14:59:31 -0700

    Refactor tests for readability

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -1,50 +1,88 @@
-# -*- coding: utf-8 -*-
-
+import os
 import sys
 from .context import wordsegment
-from wordsegment import segment, main
+from wordsegment import load, main, segment
+
+load()
 
 def test_segment_0():
-    assert segment('choosespain') == ['choose', 'spain']
+    result = ['choose', 'spain']
+    assert segment(''.join(result)) == result
 
 def test_segment_1():
-    assert segment('thisisatest') == ['this', 'is', 'a', 'test']
+    result = ['this', 'is', 'a', 'test']
+    assert segment(''.join(result)) == result
 
 def test_segment_2():
-    assert segment('wheninthecourseofhumaneventsitbecomesnecessary') == ['when', 'in', 'the', 'course', 'of', 'human', 'events', 'it', 'becomes', 'necessary']
+    result = [
+        'when', 'in', 'the', 'course', 'of', 'human', 'events', 'it',
+        'becomes', 'necessary'
+    ]
+    assert segment(''.join(result)) == result
 
 def test_segment_3():
-    assert segment('whorepresents') == ['who', 'represents']
+    result = ['who', 'represents']
+    assert segment(''.join(result)) == result
 
 def test_segment_4():
-    assert segment('expertsexchange') == ['experts', 'exchange']
+    result = ['experts', 'exchange']
+    assert segment(''.join(result)) == result
 
 def test_segment_5():
-    assert segment('speedofart') == ['speed', 'of', 'art']
+    result = ['speed', 'of', 'art']
+    assert segment(''.join(result)) == result
 
 def test_segment_6():
-    assert segment('nowisthetimeforallgood') == ['now', 'is', 'the', 'time', 'for', 'all', 'good']
+    result = ['now', 'is', 'the', 'time', 'for', 'all', 'good']
+    assert segment(''.join(result)) == result
 
 def test_segment_7():
-    assert segment('itisatruthuniversallyacknowledged') == ['it', 'is', 'a', 'truth', 'universally', 'acknowledged']
+    result = ['it', 'is', 'a', 'truth', 'universally', 'acknowledged']
+    assert segment(''.join(result)) == result
 
 def test_segment_8():
-    assert segment('itwasabrightcolddayinaprilandtheclockswerestrikingthirteen') == ['it', 'was', 'a', 'bright', 'cold', 'day', 'in', 'april', 'and', 'the', 'clocks', 'were', 'striking', 'thirteen']
+    result = [
+        'it', 'was', 'a', 'bright', 'cold', 'day', 'in', 'april', 'and', 'the',
+        'clocks', 'were', 'striking', 'thirteen'
+    ]
+    assert segment(''.join(result)) == result
 
 def test_segment_9():
-    assert segment('itwasthebestoftimesitwastheworstoftimesitwastheageofwisdomitwastheageoffoolishness') == ['it', 'was', 'the', 'best', 'of', 'times', 'it', 'was', 'the', 'worst', 'of', 'times', 'it', 'was', 'the', 'age', 'of', 'wisdom', 'it', 'was', 'the', 'age', 'of', 'foolishness']
+    result = [
+        'it', 'was', 'the', 'best', 'of', 'times', 'it', 'was', 'the', 'worst',
+        'of', 'times', 'it', 'was', 'the', 'age', 'of', 'wisdom', 'it', 'was',
+        'the', 'age', 'of', 'foolishness'
+    ]
+    assert segment(''.join(result)) == result
 
 def test_segment_10():
-    assert segment('asgregorsamsaawokeonemorningfromuneasydreamshefoundhimselftransformedinhisbedintoagiganticinsect') == ['as', 'gregor', 'samsa', 'awoke', 'one', 'morning', 'from', 'uneasy', 'dreams', 'he', 'found', 'himself', 'transformed', 'in', 'his', 'bed', 'into', 'a', 'gigantic', 'insect']
+    result = [
+        'as', 'gregor', 'samsa', 'awoke', 'one', 'morning', 'from', 'uneasy',
+        'dreams', 'he', 'found', 'himself', 'transformed', 'in', 'his', 'bed',
+        'into', 'a', 'gigantic', 'insect'
+    ]
+    assert segment(''.join(result)) == result
 
 def test_segment_11():
-    assert segment('inaholeinthegroundtherelivedahobbitnotanastydirtywetholefilledwiththeendsofwormsandanoozysmellnoryetadrybaresandyholewithnothinginittositdownonortoeatitwasahobbitholeandthatmeanscomfort') == ['in', 'a', 'hole', 'in', 'the', 'ground', 'there', 'lived', 'a', 'hobbit', 'not', 'a', 'nasty', 'dirty', 'wet', 'hole', 'filled', 'with', 'the', 'ends', 'of', 'worms', 'and', 'an', 'oozy', 'smell', 'nor', 'yet', 'a', 'dry', 'bare', 'sandy', 'hole', 'with', 'nothing', 'in', 'it', 'to', 'sit', 'down', 'on', 'or', 'to', 'eat', 'it', 'was', 'a', 'hobbit', 'hole', 'and', 'that', 'means', 'comfort']
+    result = [
+        'in', 'a', 'hole', 'in', 'the', 'ground', 'there', 'lived', 'a',
+        'hobbit', 'not', 'a', 'nasty', 'dirty', 'wet', 'hole', 'filled', 'with',
+        'the', 'ends', 'of', 'worms', 'and', 'an', 'oozy', 'smell', 'nor',
+        'yet', 'a', 'dry', 'bare', 'sandy', 'hole', 'with', 'nothing', 'in',
+        'it', 'to', 'sit', 'down', 'on', 'or', 'to', 'eat', 'it', 'was', 'a',
+        'hobbit', 'hole', 'and', 'that', 'means', 'comfort'
+    ]
+    assert segment(''.join(result)) == result
 
 def test_segment_12():
-    assert segment('faroutintheunchartedbackwatersoftheunfashionableendofthewesternspiralarmofthegalaxyliesasmallunregardedyellowsun') == ['far', 'out', 'in', 'the', 'uncharted', 'backwaters', 'of', 'the', 'unfashionable', 'end', 'of', 'the', 'western', 'spiral', 'arm', 'of', 'the', 'galaxy', 'lies', 'a', 'small', 'un', 'regarded', 'yellow', 'sun']
+    result = [
+        'far', 'out', 'in', 'the', 'uncharted', 'backwaters', 'of', 'the',
+        'unfashionable', 'end', 'of', 'the', 'western', 'spiral', 'arm', 'of',
+        'the', 'galaxy', 'lies', 'a', 'small', 'un', 'regarded', 'yellow', 'sun'
+    ]
+    assert segment(''.join(result)) == result
 
 def test_main():
     main(['tests/test.txt'])
-    import os
     result = os.linesep.join(('choose spain', 'this is a test')) + os.linesep
     assert sys.stdout.getvalue() == result
