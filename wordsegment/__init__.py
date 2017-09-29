4e667c3ee96806356967f5ea0578688182f24f5b
Grant Jenks <contact@grantjenks.com>
2017-09-29 16:38:45 -0700

    Bump version to 1.1.5

--- wordsegment/__init__.py
+++ wordsegment/__init__.py
@@ -217,8 +217,8 @@ __all__ = [
     'main'
 ]
 __title__ = 'wordsegment'
-__version__ = '1.1.4'
-__build__ = 0x010104
+__version__ = '1.1.5'
+__build__ = 0x010105
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2017 Grant Jenks'
